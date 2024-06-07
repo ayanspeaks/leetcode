42d2d2dd2d8c9c1f066c29e6d6d4c0624916623b
Ayan Sardar <97190014+ayanspeaks@users.noreply.github.com>
2024-06-07 16:35:51 +0530

    Time: 26 ms (81.11%), Space: 22.9 MB (54.98%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.cpp
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.cpp
@@ -1,38 +1,74 @@
 class Solution {
+    // This is the reference map.
+    std::unordered_map<std::string, unsigned int> map;
 public:
-    
-    bool checkSubstring(unordered_map<string, int> wordCount, string s, int wordLen) {
-        for(int j=0; j<s.size(); j+=wordLen) {
-            string w = s.substr(j, wordLen);
-            if(wordCount.find(w) != wordCount.end()) {
-                if(--wordCount[w] == -1) {
-                    return false;
-                }
-            } else {
-                return false;
-            }
-        }
-        return true;
-    }
-
     vector<int> findSubstring(string s, vector<string>& words) {
-        vector<int> res;
-        int wordLen = words[0].size();
-        int sLen = s.size();
-        int wordsWindow = words.size() * wordLen;
-        
-        unordered_map<string, int> wordCount;
-        for(int i=0; i<words.size(); i++) {
-            wordCount[words[i]]++;
-        }
-        
-        int i = 0;
-        while(i + wordsWindow <= sLen) {
-            if(checkSubstring(wordCount, s.substr(i, wordsWindow), wordLen)) {
-                res.push_back(i);
+        std::vector<int> result;
+
+        // Length of each word in words.
+        unsigned int length = words[0].size();
+
+        // Initializing the reference map.
+        map.clear();
+        for (const std::string& word : words)
+            map[word]++;
+
+        // Iterate for each offset
+        // As many times as the number of characters in each word.
+        for (unsigned int offset = 0; offset < length; ++offset) {
+            // Sliding window size.
+            unsigned int size = 0;
+            // Sliding window memory.
+            std::unordered_map<std::string, unsigned int> seen;
+
+            // Iterate over the string, with the step equals to length.
+            for (unsigned int i = offset; i + length <= s.size(); i += length) {
+                // This is the word we're going to check.
+                std::string sub = s.substr(i, length);
+
+                // If the word is absent in the reference map,
+                // we clear the sliding window and move on.
+                auto itr = map.find(sub);
+                if (itr == map.end()) {
+                    seen.clear();
+                    size = 0;
+                    continue;
+                }
+
+                // Increase the number of occurrences
+                // of the word in seen map.
+                ++seen[sub];
+                ++size;
+
+                // To make sure the sliding window is valid,
+                // we need to check only the recent occurrence
+                // against the reference,
+                // because previous occurrences
+                // were checked on previous iterations.
+                while (seen[sub] > itr->second) {
+                    // If the occurrences amount exceeds
+                    // the reference amount,
+                    // we shrink the window from the left until
+                    // the window becomes valid again.
+
+                    // A word at the beginning of the current sliding window.
+                    std::string first = s.substr(i - (size - 1) * length, length);
+
+                    // Remove the occurrence from the window,
+                    // shrinking it from the left.
+                    --seen[first];
+                    --size;
+                }
+                
+                // If we used all words from the words array,
+                // we have found the correct spot
+                // and we need to calculate the beginning
+                // index of the current sliding window.
+                if (size == words.size())
+                    result.push_back(i - (size - 1) * length);
             }
-            i++;
         }
-        return res;
+
+        return result;
     }
 };
