ccaa2de60e42152665d8282b5e575a7f535c6b7e
Ayan Sardar <97190014+ayanspeaks@users.noreply.github.com>
2024-06-13 12:29:07 +0530

    Time: 6 ms (64.15%), Space: 8.3 MB (76.54%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        int len = s.size();
-        char seen[128] = {};
-        for (int i = 0; i < len; i++) {
-            char c = s[i];
-            if (!seen[c]) {
-                for (char s: seen) if (s == t[i]) return false;
-                seen[c] = t[i];
-            }
-            else if (seen[c] != t[i]) return false;
+        vector<int> indexS(200, 0); // Stores index of characters in string s
+        vector<int> indexT(200, 0); // Stores index of characters in string t
+        
+        int len = s.length(); // Get the length of both strings
+        
+        if(len != t.length()) { // If the lengths of the two strings are different, they can't be isomorphic
+            return false;
         }
-        return true;
+        
+        for(int i = 0; i < len; i++) { // Iterate through each character of the strings
+            if(indexS[s[i]] != indexT[t[i]]) { // Check if the index of the current character in string s is different from the index of the corresponding character in string t
+                return false; // If different, strings are not isomorphic
+            }
+            
+            indexS[s[i]] = i + 1; // updating position of current character
+            indexT[t[i]] = i + 1;
+        }
+        
+        return true; // If the loop completes without returning false, strings are isomorphic
     }
 };
