5eb3671e26feeb6cb783f590339d59eeda617a4a
Ayan Sardar <97190014+ayanspeaks@users.noreply.github.com>
2024-06-15 01:55:14 +0530

    Time: 28 ms (57.03%), Space: 25 MB (25.96%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
         
-     ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        
-        unordered_map<string,vector<string> > mymap;
-        int n = strs.size();
-        string temp;
-        
-        for(int i =0;i<n;++i)
-        {
-            temp = strs[i];
-            sort(strs[i].begin(),strs[i].end());
-            mymap[strs[i]].push_back(temp);
+        for(auto x: strs){
+            string word = x;
+            sort(word.begin(), word.end());
+            mp[word].push_back(x);
         }
         
-        vector<vector<string> > result;
-        
-        for(auto itr=mymap.begin();itr!=mymap.end();++itr)
-            result.push_back(itr->second);
-        
-        return result;
+        vector<vector<string>> ans;
+        for(auto x: mp){
+            ans.push_back(x.second);
+        }
+        return ans;
     }
 };
