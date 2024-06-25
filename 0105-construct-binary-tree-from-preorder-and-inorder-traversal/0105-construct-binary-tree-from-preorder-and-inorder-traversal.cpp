a53a5aefda86f6813b3b69748a5ac6bba2a544ed
Ayan Sardar <97190014+ayanspeaks@users.noreply.github.com>
2024-06-25 11:56:11 +0530

    Time: 7 ms (91.31%), Space: 26.1 MB (34.21%) - LeetHub

---
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int preInd = 0;
+    unordered_map<int, int> inMap;  // HashMap to store inorder value -> index mapping
+    
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        // Build the hashmap to quickly find the root position in inorder array
+        for (int i = 0; i < inorder.size(); ++i) {
+            inMap[inorder[i]] = i;
+        }
+        return createTree(preorder, 0, inorder.size() - 1);
+    }
+
+    TreeNode* createTree(vector<int>& preorder, int start, int end) {
+        if (start > end) {
+            return NULL;
+        }
+
+        // The current root value is the current element in preorder
+        TreeNode* node = new TreeNode(preorder[preInd++]);
+
+        // Find the index of this root in inorder array
+        int pos = inMap[node->val];
+
+        // Recursively build the left and right subtrees
+        node->left = createTree(preorder, start, pos - 1);
+        node->right = createTree(preorder, pos + 1, end);
+
+        return node;
+    }
+};
