cf356a2c6edeefa5da8569f70a913eabf4d2f0b8
Ayan Sardar <97190014+ayanspeaks@users.noreply.github.com>
2024-07-05 12:23:26 +0530

    Time: 189 ms (9.46%), Space: 121 MB (5.36%) - LeetHub

---
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> nodesBetweenCriticalPoints(ListNode* head) {
+          vector<int> ans(2,-1);
+        if(head==NULL || head->next==NULL || head->next->next==NULL || head->next->next->next==NULL){
+            return ans;
+        }
+        ListNode* prev=head;
+        ListNode* nex=head->next->next;
+        ListNode* cur=head->next;
+        int index=0;
+        vector<int> points;
+        while(nex){
+            if(cur->val<prev->val && cur->val<nex->val){
+                 points.push_back(index);
+            }
+            if(cur->val>prev->val && cur->val>nex->val){
+                 points.push_back(index);
+            }
+            index++;
+            prev=prev->next;
+            cur=cur->next;
+            nex=nex->next;
+
+        }
+        int minn= INT_MAX;
+        int maxx=INT_MIN;
+        if(points.size()<2){
+            return ans;
+        }
+            sort(points.begin(),points.end());
+            for(int i=0;i<points.size()-1;i++){
+                minn=min(minn,abs(points[i]-points[i+1]));
+            }
+            maxx=abs(points[0]-points[points.size()-1]);
+            ans[0]=minn;
+            ans[1]=maxx;
+      return ans;
+    }
+};
