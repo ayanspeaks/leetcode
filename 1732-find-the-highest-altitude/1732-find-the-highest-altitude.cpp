7fc58029ac8217d64668956fe2e92560583620d8
Ayan Sardar <97190014+ayanspeaks@users.noreply.github.com>
2024-07-18 12:25:14 +0530

    Time: 5 ms (13.83%), Space: 10.4 MB (11.38%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -7,4 +7,20 @@ public:
             alt[i+1] = alt[i] + gain[i];
         return *max_element(alt.begin(), alt.end());
     }
-};
+};// o(n), o(n)
+
+/*class Solution {
+public:
+   int largestAltitude(vector<int>& gain) {
+        int maxAltitude = 0, currentAltitude = 0;
+        
+        for (int g : gain) {
+            currentAltitude += g;
+            if (currentAltitude > maxAltitude)
+                maxAltitude = currentAltitude;
+        }
+        
+        return maxAltitude;
+    }
+}; o(n), o(1)*/
+
