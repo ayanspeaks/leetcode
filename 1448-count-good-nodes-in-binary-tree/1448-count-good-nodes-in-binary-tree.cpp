bdfa298f31c329522f530071830c46c533f31ac8
Ayan Sardar <97190014+ayanspeaks@users.noreply.github.com>
2024-07-23 11:10:08 +0530

    Time: 90 ms (81.87%), Space: 84.9 MB (21.12%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
@@ -11,19 +11,26 @@
  */
 class Solution {
 public:
-    int count = 0;
-    void dfs(TreeNode* node,int curMax){
-        if (!node) return;
+    int count = 0;  // Global variable to keep track of the number of good nodes
+    
+    // Helper function to perform DFS
+    void dfs(TreeNode* node, int curMax) {
+        if (!node) return;  // Base case: if the node is null, return
         
-        if (node->val >= curMax){
-            count++;
-            curMax = node->val;
+        // If the current node's value is greater than or equal to the current maximum, it is a good node
+        if (node->val >= curMax) {
+            count++;  // Increment the count of good nodes
+            curMax = node->val;  // Update the current maximum value
         }
+        
+        // Recursively call dfs on the left and right children with the updated current maximum value
         dfs(node->left, curMax);
         dfs(node->right, curMax);
     }
+    
+    // Main function to count the number of good nodes
     int goodNodes(TreeNode* root) {
-        dfs(root, root->val);
-        return count;
+        dfs(root, root->val);  // Start the DFS traversal from the root node
+        return count;  // Return the count of good nodes
     }
 };
