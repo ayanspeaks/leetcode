502b0853a51c9d2d5c4d3b2f3cc169d6f4faa64c
Ayan Sardar <97190014+ayanspeaks@users.noreply.github.com>
2024-07-25 11:53:02 +0530

    Time: 7 ms (23.87%), Space: 12.4 MB (13.51%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,3 +1,6 @@
+#include <vector>
+using namespace std;
+
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
@@ -5,24 +8,28 @@ public:
         int high = nums.size() - 1;
         int mid;
 
+        // If target is greater than the last element, it should be inserted at the end
         if (target > nums[high]) {
             return high + 1;
         }
 
+        // Binary search
         while (low <= high) {
-            mid = low + (high - low) / 2; // Avoids overflow
+            mid = low + (high - low) / 2; // Calculate mid to avoid overflow
 
             if (nums[mid] == target) {
-                return mid;
+                return mid; // Target found
             }
 
             if (target < nums[mid]) {
-                high = mid - 1;
+                high = mid - 1; // Search in the left half
             } else {
-                low = mid + 1;
+                low = mid + 1; // Search in the right half
             }
         }
 
+        // Return the insert position
         return low;
     }
 };
+
