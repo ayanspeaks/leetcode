86948d3c16b33392079d220497c93da8cae28272
Ayan Sardar <97190014+ayanspeaks@users.noreply.github.com>
2024-07-26 11:58:17 +0530

    Time: 0 ms (100.00%), Space: 9.3 MB (42.96%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int> ans(n + 1, 0);
+        vector<int> ans(n + 1, 0); // Initialize a vector of size n+1 with all zeros
         
         for (int i = 1; i <= n; ++i) {
+            // ans[i >> 1] gives the number of 1-bits in i/2
+            // (i & 1) checks if the least significant bit of i is 1
             ans[i] = ans[i >> 1] + (i & 1);
         }
         
-        return ans;
+        return ans; // Return the result vector
     }
 };
