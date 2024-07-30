fb41f8c54eb4cb43482845a880e33d080070700e
Ayan Sardar <97190014+ayanspeaks@users.noreply.github.com>
2024-07-30 11:34:16 +0530

    Time: 24 ms (16.02%), Space: 28.1 MB (7.21%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,8 +1,12 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-       sort(nums.begin(),nums.end());
-       int n = nums.size();
-       return nums[n/2];
+        // Step 1: Sort the array
+        sort(nums.begin(), nums.end());
+        
+        // Step 2: The middle element in a sorted array is guaranteed to be the majority element
+        // because the majority element appears more than n/2 times.
+        int n = nums.size();
+        return nums[n / 2];
     }
 };
