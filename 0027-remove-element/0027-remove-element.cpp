1836fe5b9f24223ee01654d5f5b817c501eeaf16
Ayan Sardar <97190014+ayanspeaks@users.noreply.github.com>
2024-07-30 11:16:16 +0530

    Time: 0 ms (100.00%), Space: 10.8 MB (11.90%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-       int index=0;
+        int index = 0;  // Index to place the next non-val element
         
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i] != val){
+        // Iterate through the elements in the array
+        for (int i = 0; i < nums.size(); i++) {
+            // If the current element is not equal to val, place it at the index position
+            if (nums[i] != val) {
                 nums[index] = nums[i];
-                index++;
+                index++;  // Move to the next position
             }
         }
+        
+        // Return the new length of the array after removal
         return index;
     }
 };
