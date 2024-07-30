4a3b45a75338da680f2b94f6bda7ab5a05ae764f
Ayan Sardar <97190014+ayanspeaks@users.noreply.github.com>
2024-07-31 00:55:14 +0530

    Time: 0 ms (100.00%), Space: 10.2 MB (51.73%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        string res="";
+        string res = "";  // Initialize the result string
+        // Arrays to store Roman numeral symbols and their corresponding values
         string symbol[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-        int val[]={1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        int val[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
                          
-        for(int i=0; num!=0; i++){
-            while(num>=val[i]){
-                num-=val[i];
-                res+=symbol[i];
+        // Iterate through each symbol and its value
+        for(int i = 0; num != 0; i++) {
+            // While the number is greater than or equal to the current value
+            while(num >= val[i]) {
+                num -= val[i];  // Subtract the value from the number
+                res += symbol[i];  // Append the symbol to the result string
             }
         }
-        return res;
+        return res;  // Return the final Roman numeral string
     }
 };
