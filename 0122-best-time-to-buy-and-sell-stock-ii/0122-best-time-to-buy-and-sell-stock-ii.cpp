f473ae8266565b6ffb52e627a26768dd89846505
Ayan Sardar <97190014+ayanspeaks@users.noreply.github.com>
2024-07-30 12:04:49 +0530

    Time: 6 ms (53.08%), Space: 15.9 MB (51.48%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int n=prices.size(),i,p;
-        int profit=0,min=prices[0];
-        for(i=1;i<n;i++) {
-            if(prices[i]<min)
-                min=prices[i];
-            else if(prices[i]-min>0 )
-            { 
-               profit+=prices[i]-min;
-               min=prices[i];
+        int profit = 0;
+        int n = prices.size();
+        
+        for (int i = 1; i < n; i++) {
+            // If the current price is higher than the previous price, there is a profit opportunity
+            if (prices[i] > prices[i - 1]) {
+                profit += prices[i] - prices[i - 1];
             }
         }
         
