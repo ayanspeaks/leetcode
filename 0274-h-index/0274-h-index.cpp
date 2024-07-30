9ae20bbe8f951266cff03dd41add61aa16e17d72
Ayan Sardar <97190014+ayanspeaks@users.noreply.github.com>
2024-07-30 12:39:31 +0530

    Time: 5 ms (32.68%), Space: 11 MB (31.06%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -1,11 +1,19 @@
 class Solution {
 public:
-    int hIndex(vector<int>& c) {
-        sort(c.begin(),c.end());
-        int n=c.size(), i;
-        for(i=1; i<=n; ++i){
-            if(c[n-i]<i) break;
+    int hIndex(vector<int>& citations) {
+        // Sort the citations in ascending order
+        sort(citations.begin(), citations.end());
+        int n = citations.size(); // Number of papers
+        int i;
+
+        // Iterate to find the maximum H-index
+        for(i = 1; i <= n; ++i) {
+            // Check if the (n-i)th citation is less than i
+            if(citations[n - i] < i) {
+                break; // If true, we've found the H-index
+            }
         }
-        return i-1;
+        
+        return i - 1; // Return the maximum H-index
     }
 };
