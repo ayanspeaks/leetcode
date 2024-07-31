4101c02f5c04b7b94316063d3d7347cfac6f8a1e
Ayan Sardar <97190014+ayanspeaks@users.noreply.github.com>
2024-07-31 11:46:45 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (38.30%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,25 +1,32 @@
 class Solution {
 public:
-    int strStr(string h, string n) {
+    int strStr(string haystack, string needle) {
         
-        if(h.size()<n.size()){
+        // If the haystack is shorter than the needle, needle can't be found
+        if(haystack.size() < needle.size()) {
             return -1;
         }
         
-        int j=0;
-        for(int i=0; i<h.size()-n.size()+1; i++){
-            if(h[i]==n[j]){
-                int l=i;
-                while(j<n.size() && h[l]==n[j]){
+        int j = 0; // Index for needle
+        // Loop through the haystack up to the point where the remaining characters are less than the length of the needle
+        for(int i = 0; i <= haystack.size() - needle.size(); i++) {
+            // Check if the current character matches the first character of the needle
+            if(haystack[i] == needle[j]) {
+                int l = i; // Index for haystack
+                // Check the subsequent characters
+                while(j < needle.size() && haystack[l] == needle[j]) {
                     l++;
                     j++;
                 }
-                if(j==n.size()){
+                // If we've checked all characters in the needle, return the start index
+                if(j == needle.size()) {
                     return i;
                 }
             }
-            j=0;
+            // Reset needle index if full match not found
+            j = 0;
         }
+        // Return -1 if needle not found in haystack
         return -1;
     }
 };
