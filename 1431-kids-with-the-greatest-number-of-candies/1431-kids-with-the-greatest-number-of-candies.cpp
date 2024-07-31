2ad6e2fb88e1b6eff0d0afa9163dda47fce59311
Ayan Sardar <97190014+ayanspeaks@users.noreply.github.com>
2024-07-31 12:40:01 +0530

    Time: 6 ms (20.97%), Space: 11.3 MB (8.28%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
-         vector<bool> out;
-        int max = *max_element(candies.begin(), candies.end());
+        vector<bool> out;  // Output vector to store results
+        int max = *max_element(candies.begin(), candies.end());  // Find the maximum number of candies any kid has
         
-        for(int i=0; i < candies.size(); i++){
-            if(candies[i]+extraCandies >= max)
-                out.push_back(true);
+        // Loop through each kid's candies
+        for(int i = 0; i < candies.size(); i++) {
+            // Check if the current kid's candies plus extraCandies is greater than or equal to the max
+            if(candies[i] + extraCandies >= max)
+                out.push_back(true);  // This kid can have the maximum candies
             else
-                out.push_back(false);
+                out.push_back(false);  // This kid cannot have the maximum candies
         }
         
-        return out;
+        return out;  // Return the result
     }
 };
