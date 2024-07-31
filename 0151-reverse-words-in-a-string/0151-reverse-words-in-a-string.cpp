178e11394842b83dac70e05a99ddb9bbcf0d9368
Ayan Sardar <97190014+ayanspeaks@users.noreply.github.com>
2024-07-31 11:17:18 +0530

    Time: 6 ms (46.86%), Space: 8.5 MB (77.95%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,23 +1,34 @@
 class Solution {
 public:
     string reverseWords(string s) {
+        // Reverse the entire string first
         reverse(s.begin(), s.end());
+        
         int l = 0, r = 0, i = 0, n = s.size();
+
         while (i < n) {
+            // Move `r` and `i` to the end of the current word
             while (i < n && s[i] != ' ')
                 s[r++] = s[i++];
 
-            if (l < r) { // if we can find a non-empty word then
-                reverse(s.begin() + l, s.begin() + r); // reverse current word
-                if (r == n) break;
-                s[r++] = ' '; // set empty space
-                l = r;
+            if (l < r) { 
+                // If we have found a non-empty word
+                // Reverse the current word
+                reverse(s.begin() + l, s.begin() + r);
+
+                if (r == n) break; // If we've reached the end, break out
+
+                s[r++] = ' '; // Set a space after the word
+                l = r; // Move `l` to the start of the next word
             }
-            ++i; // now i == n or s[i] == ' ', so we skip that character!
+            ++i; // Skip the space
         }
-        if (r > 0 && s[r-1] == ' ') --r; // skip last empty character if have
-        s.resize(r);
-        return s;
+
+        if (r > 0 && s[r-1] == ' ') --r; // Remove trailing space if present
+
+        s.resize(r); // Resize the string to remove any extra spaces at the end
+
+        return s; // Return the result
     }
 };
 
