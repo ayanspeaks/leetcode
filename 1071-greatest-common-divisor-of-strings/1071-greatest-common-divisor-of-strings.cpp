4c1ffc1cee031a6e2c9a7308b24724bb1612d0bb
Ayan Sardar <97190014+ayanspeaks@users.noreply.github.com>
2024-07-31 12:25:36 +0530

    Time: 6 ms (23.73%), Space: 8.7 MB (22.04%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -1,7 +1,14 @@
 class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
-         return (str1 + str2 == str2 + str1)? 
-        str1.substr(0, gcd(size(str1),size(str2))): "";
+        // Check if concatenation in both orders results in the same string
+        // This ensures str1 and str2 have the same repeating pattern
+        if (str1 + str2 == str2 + str1) {
+            // Find the greatest common divisor (gcd) of the lengths of str1 and str2
+            // The substring from 0 to gcd(length of str1, length of str2) will be the result
+            return str1.substr(0, gcd(str1.size(), str2.size()));
+        }
+        // If the concatenations are not equal, there is no common divisor string
+        return "";
     }
 };
