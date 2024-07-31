5363af0b05c3574b682732fcf0c6f85b1390d92f
Ayan Sardar <97190014+ayanspeaks@users.noreply.github.com>
2024-07-31 12:14:25 +0530

    Time: 0 ms (100.00%), Space: 8 MB (19.02%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-         int n = word1.size();
-        int m = word2.size();
-        string ans = "";
+        int n = word1.size(); // Length of word1
+        int m = word2.size(); // Length of word2
+        string ans = ""; // Result string
 
+        // Merge characters alternately from both strings
         for (int i = 0; i < n && i < m; ++i) {
-            ans += word1[i];
-            ans += word2[i];
+            ans += word1[i]; // Append character from word1
+            ans += word2[i]; // Append character from word2
         }
 
+        // If word1 is longer, append the remaining characters
         if (n > m) {
             ans += word1.substr(m);
-        } else if (n < m) {
+        }
+        // If word2 is longer, append the remaining characters
+        else if (n < m) {
             ans += word2.substr(n);
         }
 
-        return ans;
+        return ans; // Return the merged string
     }
 };
