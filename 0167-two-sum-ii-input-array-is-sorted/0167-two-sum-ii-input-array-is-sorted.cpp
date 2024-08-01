1758326ac446169bdbc3bce2fbf27fdcbcdaef85
Ayan Sardar <97190014+ayanspeaks@users.noreply.github.com>
2024-08-01 11:59:32 +0530

    Time: 14 ms (18.33%), Space: 18.3 MB (7.64%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        int i=0;
-        int j=numbers.size()-1;
-        while(i<j){
-            if(numbers[i]+numbers[j]==target)
-                return{i+1, j+1};
-            if(numbers[i]+numbers[j]<target)i++;
-            else j--;
+        int i = 0;  // Initialize left pointer
+        int j = numbers.size() - 1;  // Initialize right pointer
+
+        while (i < j) {
+            // Check if the sum of elements at i and j equals the target
+            if (numbers[i] + numbers[j] == target)
+                return {i + 1, j + 1};  // Return 1-based indices if target is found
+
+            // If the sum is less than the target, move the left pointer to the right
+            if (numbers[i] + numbers[j] < target)
+                i++;
+            // If the sum is greater than the target, move the right pointer to the left
+            else
+                j--;
         }
-        return{};
+
+        return {};  // Return empty vector if no solution is found
     }
 };
