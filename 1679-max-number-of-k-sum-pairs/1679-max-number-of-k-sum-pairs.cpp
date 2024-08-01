9b794a4e9273354e62b7a7b42d8d6cc63852f2c2
Ayan Sardar <97190014+ayanspeaks@users.noreply.github.com>
2024-08-01 12:52:19 +0530

    Time: 88 ms (74.70%), Space: 62 MB (51.32%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -2,22 +2,25 @@ class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
         int n = nums.size();
-        int l = 0, r = n-1;
+        int l = 0, r = n - 1;
         int count = 0;
+        
+        // Sort the array to use two-pointer technique
         sort(nums.begin(), nums.end());
-        while (l < r)
-        {
-            if (nums[l] + nums[r] == k)
-            {
-                count++;
+        
+        // Use two pointers to find pairs
+        while (l < r) {
+            if (nums[l] + nums[r] == k) {
+                count++; // Found a pair
                 l++;
                 r--;
+            } else if (nums[l] + nums[r] < k) {
+                l++; // Increment left pointer to increase sum
+            } else {
+                r--; // Decrement right pointer to decrease sum
             }
-            else if (nums[l] + nums[r] < k)
-                l++;
-            else
-                r--;
         }
-        return count;
+        
+        return count; // Return the number of pairs found
     }
 };
