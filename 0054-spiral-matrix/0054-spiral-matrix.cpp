ac2003e53f5b1663116d5d5d7f251cc5ac1db8a0
Ayan Sardar <97190014+ayanspeaks@users.noreply.github.com>
2024-08-02 15:23:49 +0530

    Time: 3 ms (37.90%), Space: 8.5 MB (18.17%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,27 +1,36 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        vector<int> res;
-        //if(matrix.empty() || matrix[0].empty()) return res;
+        vector<int> res;  // Result vector to store the elements in spiral order
+        if (matrix.empty() || matrix[0].empty()) return res;  // Return empty vector if matrix is empty
         
-        int n = matrix.size(), m = matrix[0].size();
-        int up = 0, down = n-1, left = 0, right = m-1; 
+        int n = matrix.size(), m = matrix[0].size();  // Get the dimensions of the matrix
+        int up = 0, down = n - 1, left = 0, right = m - 1;  // Initialize boundary pointers
         
-        while(res.size() < n*m){
-            for(int j=left; j<=right && res.size()<n*m; j++)
+        // Loop until we've processed all elements
+        while (res.size() < n * m) {
+            // Traverse from left to right on the top row
+            for (int j = left; j <= right && res.size() < n * m; j++)
                 res.push_back(matrix[up][j]);
             
-            for(int i=up+1; i<=down-1 && res.size()<n*m; i++)
+            // Traverse from top to bottom on the right column
+            for (int i = up + 1; i <= down - 1 && res.size() < n * m; i++)
                 res.push_back(matrix[i][right]);
             
-            for(int j=right; j>=left && res.size()<n*m; j--)
+            // Traverse from right to left on the bottom row
+            for (int j = right; j >= left && res.size() < n * m; j--)
                 res.push_back(matrix[down][j]);
             
-            for(int i=down-1; i>=up+1 && res.size()<n*m; i--)
+            // Traverse from bottom to top on the left column
+            for (int i = down - 1; i >= up + 1 && res.size() < n * m; i--)
                 res.push_back(matrix[i][left]);
             
-            left++; right--; up++; down--;
+            // Move the boundaries inward
+            left++;
+            right--;
+            up++;
+            down--;
         }
-        return res;
+        return res;  // Return the result vector
     }
 };
