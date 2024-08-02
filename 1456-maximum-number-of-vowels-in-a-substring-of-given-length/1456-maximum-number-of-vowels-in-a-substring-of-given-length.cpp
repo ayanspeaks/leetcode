692240c32d693081fd485ac7e9cdad30b3b88924
Ayan Sardar <97190014+ayanspeaks@users.noreply.github.com>
2024-08-02 13:01:24 +0530

    Time: 27 ms (25.88%), Space: 11.5 MB (27.10%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int maxVowels(string s, int k) {
-        int n=s.size(),ans=0,cur=0;
-        unordered_map<char,int>m;
-        m['a']++,m['e']++,m['i']++,m['o']++,m['u']++;
-        for(int i=0;i<n;i++){
-            if(i<k){
-                cur+=m.count(s[i]);
+        int n = s.size(), ans = 0, cur = 0;
+        unordered_map<char, int> m;
+        m['a']++, m['e']++, m['i']++, m['o']++, m['u']++;  // Initialize the map with vowels
+        
+        for (int i = 0; i < n; i++) {
+            if (i < k) {
+                cur += m.count(s[i]);  // Count the vowels in the first window of size k
+            } else {
+                cur += m.count(s[i]) - m.count(s[i - k]);  // Slide the window: add current char, remove char left outside
             }
-            else{
-                cur+=m.count(s[i])-m.count(s[i-k]);
-            }
-            ans=max(ans,cur);
+            ans = max(ans, cur);  // Update the maximum count of vowels
         }
-        return ans;
+        
+        return ans;  // Return the maximum count of vowels in any window of size k
     }
 };
