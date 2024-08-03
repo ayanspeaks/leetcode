3898af298e69d0daa3639ddb0971b027b6d52bf4
Ayan Sardar <97190014+ayanspeaks@users.noreply.github.com>
2024-08-03 13:09:22 +0530

    Time: 13 ms (25.98%), Space: 10 MB (12.88%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        // If the lengths of the strings are not equal, they can't be anagrams
         if (s.size() != t.size()) {
             return false;
         }
 
+        // Create a map to count the occurrences of each character
         unordered_map<char, int> charCount;
 
         // Count the characters in the first string
         for (char c : s) {
             charCount[c]++;
         }
-
         // Decrease the count for the characters in the second string
         for (char c : t) {
             charCount[c]--;
