ae30b8c672d63739bb3347dbdcfe39bed8d34855
Ayan Sardar <97190014+ayanspeaks@users.noreply.github.com>
2024-08-04 15:20:41 +0530

    Time: 7 ms (67.83%), Space: 15.8 MB (15.32%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,19 +1,28 @@
 static unordered_set<string> operations({"+", "-", "*", "/"});
+
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
         stack<int> stack;
+        
         for (auto t : tokens) {
-            if (operations.find(t) == operations.end()) stack.push(stoi(t));
-            else {
-                int b = stack.top(); stack.pop();
-                int a = stack.top(); stack.pop();
+            if (operations.find(t) == operations.end()) {
+                // If the token is not an operation, convert it to an integer and push it onto the stack
+                stack.push(stoi(t));
+            } else {
+                // If the token is an operation, pop the top two elements from the stack
+                int b = stack.top(); 
+                stack.pop();
+                int a = stack.top(); 
+                stack.pop();                
+                // Perform the operation and push the result back onto the stack
                 if (t == "+") stack.push(a + b);
                 else if (t == "-") stack.push(a - b);
                 else if (t == "*") stack.push(a * b);
                 else stack.push(a / b);
             }
-        }
+        }     
+        // The result of the expression is the only element left in the stack
         return stack.top();
     }
 };
