78464118d081e1ca98c4e03bbcb1d6ff6e2bd48d
Ayan Sardar <97190014+ayanspeaks@users.noreply.github.com>
2024-08-04 11:51:34 +0530

    Time: 16 ms (23.39%), Space: 21.3 MB (12.63%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,35 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        
-        vector<vector<int>>ans;
-        
-        // 2 cases.
-        for(auto currentInterval:intervals){
-        //case 1 :non overlapping 
-        // new interval is after current interval. add current interval to the ans.
-        if(currentInterval[1]<newInterval[0]){ans.push_back(currentInterval);}
+        vector<vector<int>> ans;
 
-        // if new interval comes before current interval that means we need to make it current interval and check for overlaping with others. And our current interval will also be add to ans.
-        else if(newInterval[1]<currentInterval[0]){
-                    ans.push_back(newInterval); 
-                    //update it current interval for overlapping checking.
-                    newInterval = currentInterval;
+        // Iterate through each interval in the list
+        for(auto currentInterval : intervals) {
+            // Case 1: Non-overlapping (current interval ends before new interval starts)
+            if(currentInterval[1] < newInterval[0]) {
+                ans.push_back(currentInterval); // Add current interval to the result
+            }
+            // Case 1: Non-overlapping (new interval ends before current interval starts)
+            else if(newInterval[1] < currentInterval[0]) {
+                ans.push_back(newInterval); // Add new interval to the result
+                newInterval = currentInterval; // Update newInterval to current interval for further checking
+            }
+            // Case 2: Overlapping intervals
+            else {
+                // Merge intervals by updating the start and end of newInterval
+                newInterval[0] = min(currentInterval[0], newInterval[0]);
+                newInterval[1] = max(currentInterval[1], newInterval[1]);
+            }
         }
-        // case 2 :overlapping
-        else{
-            // update the newInterval for checking overlapping.
-            newInterval[0] = min(currentInterval[0],newInterval[0]);
-            newInterval[1] = max(currentInterval[1],newInterval[1]);
-
-        }
-
-        }
-        // Add the last non overlaping interval (newInterval)
+        // Add the last interval (newInterval)
         ans.push_back(newInterval);
 
-        return ans;
-
-        
+        return ans; // Return the result
     }
 };
