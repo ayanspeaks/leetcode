ec1929498ae466bbebf7a44cb39b9366edd5febe
Ayan Sardar <97190014+ayanspeaks@users.noreply.github.com>
2024-08-04 18:10:46 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (33.32%) - LeetHub

--- 0394-decode-string/0394-decode-string.cpp
+++ 0394-decode-string/0394-decode-string.cpp
@@ -1,60 +1,52 @@
 class Solution {
+    // Starting index of the encoded string
+    int start = 0;
 
-   // starting Index of encoding string
-   int start = 0;
+    // Recursive function to decode the string
+    string Solve(string &s) {
+        // Variable to store our answer
+        string ans = "";
 
-   string Solve(string &s )
-   {
-       // Variable To store our answer
-       string ans = "";
+        // Traverse forward till we don't get a numeric character
+        // or closing bracket or we reach the end
+        while (start < s.length() && s[start] >= 'a' && s[start] <= 'z' && s[start] != ']') {
+            ans.push_back(s[start++]);
+        }
 
-      //We will Traverse forward till wo dont get a numeric alphabet 
-      // or closing bracket or we reach till end
+        // If we reach the end or we are at a closing bracket, we return the ans as it is
+        if (start >= s.length() || s[start] == ']') {
+            return ans;
+        }
 
-       while( start < s.length() && s[start] >= 'a' && s[start]<= 'z' && s[start] != ']' )
-       ans.push_back(s[start++]);
-       
-     // if We reach end or we are at closing bracket we return 
-     // our ans as it is 
-     // eg1 : string = abcdef
-     // we will have reache the end after starting from 0
-     
-     // eg2 : string = ab2[def]ghi
-     // we will have reach at ']' after starting from 'd'
-     
-    
-       if(start >= s.length() || s[start] == ']')
-       return ans;
-    
-     // We have reach the numerical character
-       string num = "";
-     
-     // store the number to be repeated
-       while(s[start] != '[' )
-       num.push_back(s[start++]);
-       int n = stoi(num) ;
-     
-     // Jump one index from '[' and get the repeating substring 
-       start++;
-    
-       string repeat = Solve(s );
-    // Attach the repeating substring to our ans
-      for(int k = 0 ; k< n ; k++)
-           ans+= repeat;
+        // We have reached a numeric character
+        string num = "";
 
-     // Jump one index from ']' and get the substring ahead of
-     //  repeating substring if any 
+        // Store the number to be repeated
+        while (s[start] != '[') {
+            num.push_back(s[start++]);
+        }
+        int n = stoi(num);
 
-       start++;
-     
-     //Return ans
+        // Jump one index from '[' and get the repeating substring
+        start++;
+
+        string repeat = Solve(s);
+
+        // Attach the repeating substring to our ans
+        for (int k = 0; k < n; k++) {
+            ans += repeat;
+        }
+
+        // Jump one index from ']' and get the substring ahead of
+        // the repeating substring if any
+        start++;
+
+        // Return ans
         return ans + Solve(s);
-        
-}
+    }
 
 public:
     string decodeString(string s) {
-        
         return Solve(s);
     }
 };
