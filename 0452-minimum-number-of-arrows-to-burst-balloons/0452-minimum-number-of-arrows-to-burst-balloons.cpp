b1a78ba4fb539d65692adfcf7ffab32efda52258
Ayan Sardar <97190014+ayanspeaks@users.noreply.github.com>
2024-08-04 12:02:32 +0530

    Time: 235 ms (90.74%), Space: 93.6 MB (45.21%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -1,23 +1,25 @@
-bool comp(vector<int> &a, vector<int> &b){
-    return a[1] < b[1];
+bool comp(vector<int> &a, vector<int> &b) {
+    return a[1] < b[1]; // Comparator to sort intervals based on their end points
 }
-
 
 class Solution {
 public:
     int findMinArrowShots(vector<vector<int>>& points) {
-        if(points.size() == 0) return 0;
+        if(points.size() == 0) return 0; // If there are no intervals, return 0
         
+        // Sort the points based on their end points
         sort(points.begin(), points.end(), comp);
-        int arrows=1;
-        int end=points[0][1];
         
-        for(int i=1; i<points.size(); i++){
-            if(points[i][0]>end){
-                arrows++;
-                end=points[i][1];
+        int arrows = 1; // Start with one arrow
+        int end = points[0][1]; // End point of the first interval
+        
+        for(int i = 1; i < points.size(); i++) {
+            // If the start of the current interval is greater than the end of the last interval
+            if(points[i][0] > end) {
+                arrows++; // We need a new arrow
+                end = points[i][1]; // Update the end to the end of the current interval
             }
         }
-        return arrows;
+        return arrows; // Return the minimum number of arrows needed
     }
 };
