e4de2995ccc7679c264712070d6211e9c4de5373
Ayan Sardar <97190014+ayanspeaks@users.noreply.github.com>
2024-08-07 12:02:17 +0530

    Time: 0 ms (100.00%), Space: 14.2 MB (60.96%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -11,19 +11,23 @@
  */
 class Solution {
 public:
-    void solve(TreeNode* root, int level, vector<int> &ans) {
+    void solve(TreeNode* root, int level, vector<int>& ans) {
         if (root == NULL) {
-            return;
+            return;  // Base case: if the node is null, return
         }
+        // If the current level is equal to the size of ans, add the node's value to ans
         if (level == ans.size()) {  
             ans.push_back(root->val);
         }
+        // Traverse the right subtree first
         solve(root->right, level + 1, ans);
+        // Traverse the left subtree next
         solve(root->left, level + 1, ans);
     }
+
     vector<int> rightSideView(TreeNode* root) {
-        vector<int> ans;
-        solve(root, 0, ans);
-        return ans;
+        vector<int> ans;  // Vector to store the right side view
+        solve(root, 0, ans);  // Initialize DFS from the root node with level 0
+        return ans;  // Return the vector containing the right side view
     }
 };
