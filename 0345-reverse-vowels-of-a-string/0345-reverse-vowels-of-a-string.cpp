fd23a114af81c055ac675785fa8c0e1a21b38643
Ayan Sardar <97190014+ayanspeaks@users.noreply.github.com>
2024-08-16 17:13:57 +0530

    Time: 4 ms (82.48%), Space: 9.9 MB (27.54%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -1,29 +1,35 @@
 class Solution {
 public:
     string reverseVowels(string s) {
-          int n=s.length();
-        if(n==1)
+        int n = s.length();
+        if (n == 1) // If the string has only one character, return it as is
             return s;
-        vector<bool> vis(n,false);
-        string vow="";
-        for(int i=0;i<n;i++)
-        {
-            if(s[i]=='a'||s[i]=='A'||s[i]=='i'||s[i]=='I'||s[i]=='e'||s[i]=='E'||s[i]=='o'||s[i]=='O'||s[i]=='u'||s[i]=='U')
-            {
-                vis[i]=true;
-                vow+=s[i];
+
+        vector<bool> vis(n, false); // Boolean vector to mark positions of vowels
+        string vow = ""; // String to store all vowels in the original string
+
+        // Identify all vowels and store them in 'vow' while marking their positions in 'vis'
+        for (int i = 0; i < n; i++) {
+            if (s[i] == 'a' || s[i] == 'A' || s[i] == 'i' || s[i] == 'I' || 
+                s[i] == 'e' || s[i] == 'E' || s[i] == 'o' || s[i] == 'O' || 
+                s[i] == 'u' || s[i] == 'U') {
+                vis[i] = true;
+                vow += s[i];
             }
         }
-        reverse(vow.begin(),vow.end());
-        int j=0;
-        for(int i=0;i<n;i++)
-        {
-            if(vis[i]==true)
-            {
-                s[i]=vow[j];
+
+        // Reverse the string containing all vowels
+        reverse(vow.begin(), vow.end());
+
+        int j = 0; // Index for the reversed vowels string
+        // Replace the vowels in the original string with the reversed vowels
+        for (int i = 0; i < n; i++) {
+            if (vis[i] == true) {
+                s[i] = vow[j];
                 j++;
             }
         }
-        return s;
+
+        return s; // Return the modified string
     }
 };
