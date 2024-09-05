43ef8fa4d1d7b60c235640289b09bdc74aef85ad
Ayan Sardar <97190014+ayanspeaks@users.noreply.github.com>
2024-09-05 11:05:59 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (65.75%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
-         uint32_t rev = 0;
+        uint32_t rev = 0;  // Initialize result to 0
 
-        for(int i = 31, j = 0; i >= 0; i--, j++) {
-            if((n & (1 << i)) != 0) {
-                rev |= (1 << j);
+        // Loop over all 32 bits of the input number
+        for (int i = 31, j = 0; i >= 0; i--, j++) {
+            // Check if the ith bit in n is set
+            if ((n & (1 << i)) != 0) {
+                // If set, set the jth bit in rev
+                rev |= (1 << j); // Update rev by setting the jth bit
             }
         }
-        return rev;
+
+        return rev;  // Return the reversed bit representation
     }
 };
