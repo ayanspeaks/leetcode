248abae9b3c4adbc2795b537a7da5e9df7cbdd5b
Ayan Sardar <97190014+ayanspeaks@users.noreply.github.com>
2024-11-10 12:25:40 +0530

    Time: 0 ms (100.00%), Space: 34.5 MB (99.11%) - LeetHub

---
+++ 3131-find-the-integer-added-to-array-i/3131-find-the-integer-added-to-array-i.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int addedInteger(vector<int>& nums1, vector<int>& nums2) {
+        int max1 = *max_element(nums1.begin(), nums1.end());       
+        int max2 = *max_element(nums2.begin(), nums2.end());  
+
+        return max2-max1;
+    }
+};
